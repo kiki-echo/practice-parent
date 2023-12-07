052ff4c9cd90b756695a31a64d1da39d72b87119
zwl <2806981156@qq.com>
2023-12-07 20:41:22 +0800

    1

--- practice/src/main/java/org/example/enums/Aircondition.java
+++ practice/src/main/java/org/example/enums/Aircondition.java
@@ -1,14 +1,17 @@
 package org.example.enums;
 
+import org.springframework.web.bind.annotation.RestController;
+
 public enum Aircondition {
     ONE{
-
+        @Override
         public void print(){
             System.out.println("one");
         };
 
     },
     TWO{
+        @Override
         public void print(){
             System.out.println("two");
         };
