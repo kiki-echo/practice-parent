51611f37811b1310bcd196dc554898c53e518a3e
zwl <2806981156@qq.com>
2023-12-07 16:54:45 +0800

    Initial commit

---
+++ practice/src/main/java/org/example/contoller/EnumController.java
@@ -0,0 +1,15 @@
+package org.example.contoller;
+
+
+import org.example.enums.Airondition;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EnumController {
+
+    public static void main(String[] args) {
+        System.out.println(    Airondition.one
+);
+
+    }
+}
---
+++ practice/src/main/java/org/example/enums/Airondition.java
@@ -0,0 +1,19 @@
+package org.example.enums;
+
+public enum Airondition {
+    one{
+
+        public void print(){
+            System.out.println("one");
+        };
+
+    },
+    two{
+        public void print(){
+            System.out.println("one");
+        };
+    };
+
+    public abstract   void  print();
+
+}
