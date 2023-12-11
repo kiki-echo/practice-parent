057a0309bfd331df6831c6f1cc34d2b0ad00ff41
zwl <2806981156@qq.com>
2023-12-11 17:12:47 +0800

    Merge remote-tracking branch 'origin/master'

--- practice/src/main/java/org/example/contoller/EnumController.java
+++ practice/src/main/java/org/example/contoller/EnumController.java
@@ -2,22 +2,12 @@ package org.example.contoller;
 
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.fasterxml.jackson.core.SerializableString;
 import org.example.domain.TProduct;
-import org.example.enums.Aircondition;
 import org.example.mapper.TProductMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -29,7 +19,7 @@ public class EnumController {
     @RequestMapping("test")
     public List<TProduct> test(){
         LambdaQueryWrapper<TProduct> lqw=new LambdaQueryWrapper<>();
-        lqw.eq(TProduct::getProductName,"20");
+        lqw.eq(TProduct::getId,20);
         List<TProduct> tProduct=tProductMapper.selectList(lqw);
         System.out.println(tProduct);
         return  tProduct;
--- practice/src/main/java/org/example/domain/TProduct.java
+++ practice/src/main/java/org/example/domain/TProduct.java
@@ -4,10 +4,9 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import lombok.Data;
-
 import java.io.Serializable;
 import java.math.BigDecimal;
+import lombok.Data;
 
 /**
  * 
@@ -25,27 +24,27 @@ public class TProduct implements Serializable {
     /**
      * 
      */
-    private String productName;
+    private String product_name;
 
     /**
      * 
      */
-    private String productTitle;
+    private String product_title;
 
     /**
      * 
      */
-    private String productImg;
+    private String product_img;
 
     /**
      * 
      */
-    private String productDetail;
+    private String product_detail;
 
     /**
      * 
      */
-    private BigDecimal productPrice;
+    private BigDecimal product_price;
 
     @TableField(exist = false)
     private static final long serialVersionUID = 1L;
@@ -63,11 +62,11 @@ public class TProduct implements Serializable {
         }
         TProduct other = (TProduct) that;
         return (this.getId() == null ? other.getId() == null : this.getId().equals(other.getId()))
-            && (this.getProductName() == null ? other.getProductName() == null : this.getProductName().equals(other.getProductName()))
-            && (this.getProductTitle() == null ? other.getProductTitle() == null : this.getProductTitle().equals(other.getProductTitle()))
-            && (this.getProductImg() == null ? other.getProductImg() == null : this.getProductImg().equals(other.getProductImg()))
-            && (this.getProductDetail() == null ? other.getProductDetail() == null : this.getProductDetail().equals(other.getProductDetail()))
-            && (this.getProductPrice() == null ? other.getProductPrice() == null : this.getProductPrice().equals(other.getProductPrice()));
+            && (this.getProduct_name() == null ? other.getProduct_name() == null : this.getProduct_name().equals(other.getProduct_name()))
+            && (this.getProduct_title() == null ? other.getProduct_title() == null : this.getProduct_title().equals(other.getProduct_title()))
+            && (this.getProduct_img() == null ? other.getProduct_img() == null : this.getProduct_img().equals(other.getProduct_img()))
+            && (this.getProduct_detail() == null ? other.getProduct_detail() == null : this.getProduct_detail().equals(other.getProduct_detail()))
+            && (this.getProduct_price() == null ? other.getProduct_price() == null : this.getProduct_price().equals(other.getProduct_price()));
     }
 
     @Override
@@ -75,11 +74,11 @@ public class TProduct implements Serializable {
         final int prime = 31;
         int result = 1;
         result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
-        result = prime * result + ((getProductName() == null) ? 0 : getProductName().hashCode());
-        result = prime * result + ((getProductTitle() == null) ? 0 : getProductTitle().hashCode());
-        result = prime * result + ((getProductImg() == null) ? 0 : getProductImg().hashCode());
-        result = prime * result + ((getProductDetail() == null) ? 0 : getProductDetail().hashCode());
-        result = prime * result + ((getProductPrice() == null) ? 0 : getProductPrice().hashCode());
+        result = prime * result + ((getProduct_name() == null) ? 0 : getProduct_name().hashCode());
+        result = prime * result + ((getProduct_title() == null) ? 0 : getProduct_title().hashCode());
+        result = prime * result + ((getProduct_img() == null) ? 0 : getProduct_img().hashCode());
+        result = prime * result + ((getProduct_detail() == null) ? 0 : getProduct_detail().hashCode());
+        result = prime * result + ((getProduct_price() == null) ? 0 : getProduct_price().hashCode());
         return result;
     }
 
@@ -90,11 +89,11 @@ public class TProduct implements Serializable {
         sb.append(" [");
         sb.append("Hash = ").append(hashCode());
         sb.append(", id=").append(id);
-        sb.append(", productName=").append(productName);
-        sb.append(", productTitle=").append(productTitle);
-        sb.append(", productImg=").append(productImg);
-        sb.append(", productDetail=").append(productDetail);
-        sb.append(", productPrice=").append(productPrice);
+        sb.append(", product_name=").append(product_name);
+        sb.append(", product_title=").append(product_title);
+        sb.append(", product_img=").append(product_img);
+        sb.append(", product_detail=").append(product_detail);
+        sb.append(", product_price=").append(product_price);
         sb.append(", serialVersionUID=").append(serialVersionUID);
         sb.append("]");
         return sb.toString();
