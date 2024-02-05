09b064f6925409c46397dceed06c6c4cfeb0c011
zwl <2806981156@qq.com>
2024-02-05 09:20:26 +0800

    Merge remote-tracking branch 'origin/master'

--- practice/src/main/java/org/example/contoller/EnumController.java
+++ practice/src/main/java/org/example/contoller/EnumController.java
@@ -8,31 +8,57 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @RestController
 public class EnumController {
     public static void main(String[] args) {
-        System.out.println("a".matches("[abc]"));
+        System.out.println("11".matches("^[11]"));
         Pattern p = Pattern.compile("\\w");
         Matcher matcher= p.matcher("afds");
         System.out.println(matcher);
         while (matcher.find()){
             System.out.println(matcher.group());
         }
+
+        Map<String,String> map =new HashMap<>();
+        map.put("a","a");
+        map.put("b","a");
+        map.put("c","a");
+        map.put("d",null);
+        Set<String> strings = map.keySet();
+        Set<Map.Entry<String, String>> entries = map.entrySet();
+        entries.forEach(m ->{
+            System.out.println(m.getKey()+"  "+m.getValue());
+        });
+
+
+
+        map.forEach((k,v) ->{
+            System.out.println(k+v);
+        });
     }
     @Resource
     TProductMapper tProductMapper ;
 
     @RequestMapping("test")
     public List<TProduct> test(){
-        LambdaQueryWrapper<TProduct> lqw=new LambdaQueryWrapper<>();
-        lqw.eq(TProduct::getId,20);
-        List<TProduct> tProduct=tProductMapper.selectList(lqw);
-        System.out.println(tProduct);
-        return  tProduct;
+
+        new Thread(){
+            @Override
+            public void run() {
+                LambdaQueryWrapper<TProduct> lqw=new LambdaQueryWrapper<>();
+                lqw.eq(TProduct::getId,20);
+                List<TProduct> tProduct=tProductMapper.selectList(lqw);
+                System.out.println(tProduct);
+            }
+        }.start();
+
     }
 
 
